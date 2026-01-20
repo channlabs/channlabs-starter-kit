162ce6e3aae7986445d64f77e47ce5da33d8a544
Chann Labs Home <channxnetwork@gmail.com>
2026-01-21 06:16:03 +0700

    chore: remove unused SetupProject command and update composer scripts

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
+
+
+Auth::loginUsingId(1);
 
 Route::get('/', function () {
     return Inertia::render('welcome', [
